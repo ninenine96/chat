c874535f830fb8ada1d64d7b47228a4255df5937
Mohammad R <mohammadruhaan@gmail.com>
2022-03-08 23:34:12 +0530

    first commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import axios from "axios";
+
+const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = {
+            "Project-ID": "86ba96ed-9556-47c9-aad6-39f2d4e6a2c4",
+            "User-Name": username,
+            "User-Secret": password,
+        };
+
+        try {
+            await axios.get("https://api.chatengine.io/chats", {
+                headers: authObject,
+            });
+
+            localStorage.setItem("username", username);
+            localStorage.setItem("password", password);
+
+            window.location.reload();
+        } catch (error) {
+            setError("Sadge typing bruv");
+        }
+    };
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        className="input"
+                        placeholder="Username"
+                        required
+                    />
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="input"
+                        placeholder="password"
+                        required
+                    />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                    <h2 className="error">{error}</h2>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginForm;
